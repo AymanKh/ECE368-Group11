1313414ac6f839c3214c87732e5942bed1dd57bc
Robert Schwieterman <rschwiet@purdue.edu>
2016-04-27 20:16:48 -0400

    added unstable function to test node popularity

--- graphizer.cpp
+++ graphizer.cpp
@@ -50,59 +50,82 @@ void parseline(string line, gnode graph)
 	}
 }
 
-int *traveled(struct gnode graph[], int tally[24])
+int *traveled(struct gnode GRAPH[], int tally[24])
 {
 	int dist[24] = { INF };
-	int visit[24] = { 0 };
+	int Visited[24] = { 0 };
 	int previous[24] = { -1 };
 	int k = 0;
 	int source;
-	for (k = 0; k < 24; k++)
+	for (k = 0; k < 24; k++)//for each start point
 	{
-		int clearindex=0;
-		while (clearindex < 24)
+		int u = k;
+		int loopi = 0;
+		int exit = 0;
+		double min = INF;
+		int minindex;
+		//clear prev, dist, visited
+		int ci = 0;
+		for (ci = 0; ci < 24; ci++)
 		{
-			dist[clearindex] = INF;
-			visit[clearindex] = 0;
-			previous[clearindex] = -1;
-			clearindex++;
+			dist[ci] = INF;
+			Visited[ci] = 0;
+			previous[ci] = -1;
 		}
-		source = k;
-		dist[source] = 0;
-		int z = 0;
-		for (z = 0; z < 24; z++)
-		{
-			if (graph[source].fnums[z] != 0)
-			{
-				dist[z] = 10 - graph[source].fnums[z];
-				previous[z] = source;
-			}
-		}
-		visit[source] = 1;
-		//now do this same process 24 times
-		int i = 1;
-		int least=INF;
-		int leastaddress=-1;
-		for (i = 1; i < 24; i++)
-		{
-			//find least unvisited node left
-			for (int ii = 1; i < 24; i++)
-			{
-				if ((!visit[ii])&&(dist[ii] < least))
-				{
-					least = dist[ii];
-					leastaddress = ii;
-				}
-			}
-			//update leastaddress' neighbors distances
-			for (z = 0; z < 24; z++)
-			{
-				if (graph[leastaddress].fnums[z] != 0)//found valid neighbor
-				{
-					previous[z] = source;
-				}
-			}
+		//prev, dist, visited cleared and ready to repopulate from a new start point
 
+		while (!exit) {
+			loopi = 0;
+			Visited[u] = 1;
+			//printf("arrived at new node %d AKA %s\n",u,Territories[u].name);
+			while (loopi<24)
+			{
+				if (GRAPH[u].fnums[loopi] != 0)//we have found a neighbor
+				{
+					if (dist[loopi] > dist[u] + (10-GRAPH[u].fnums[loopi]))
+					{
+						//printf("updating %s %f to %f\n",Territories[loopi].name,Dist$
+						dist[loopi] = dist[u] + (10 - GRAPH[u].fnums[loopi]);
+						previous[loopi] = u;
+					}
+				}
+				loopi++;
+			}//now updated all neighbours distances+previous's
+			 //find next node to go to
+			 //printf("\nFinding NEXT\n");
+			loopi = 0;
+			min = INF;
+			minindex = 0;
+			exit = 1;//unless we find one to go to, exit
+			while (loopi < 24)
+			{
+				if (dist[loopi] != INF) {
+					if ((dist[loopi] < min) && (!Visited[loopi]))
+					{
+						exit = 0;//we found a node to go to, don't exit
+								 //printf("id=%d distance=%f visited  %d min=%f\n",loopi,Distance_From_$
+						min = dist[loopi];
+						minindex = loopi;
+					}
+				}
+				loopi++;
+			}
+			//now have next node to go to
+			u = minindex;
+		}//prev needs to be tallied
+		int n = 0;
+		for (n = 0; n < 24; n++)
+		{
+			int p = n;
+			while (previous[p] != -1)
+			{
+				p = previous[p];
+				tally[p]++;
+			}
+		}
+		for (n = 0; n < 24; n++)
+		{
+			printf("node=%d,tally=%d\n", n, tally[n]);
 		}
 	}
 
